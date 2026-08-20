0cd12913c42fbdcc361b64aafc274e0617716843
Lazarus1_3 <eshetealazar11@gmail.com>
2026-08-20 23:44:09 +0300

    dataSet Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,42 @@ import SideBar from "./components/SideBar.jsx";
 import { ConversationTitle, chatHistory } from "./data/chatHistory.js";
 function App() {
   const [selectedChat, setSelectedChat] = useState(null);
+  const [history, setHistory] = useState(ConversationTitle.items);
+  const [message, setMessage] = useState(chatHistory);
+  const handleSubmit = (userText) => {
+    const chatId = crypto.randomUUID();
+    const addTitle = [{ id: chatId, title: userText }, ...history];
+    const addChat = [
+      {
+        title: userText,
+        conversation_id: chatId,
+        mapping: {
+          chatId: {
+            id: chatId,
+            message: {
+              id: chatId,
+              content: {
+                content_type: "text",
+                parts: [userText],
+              },
+            },
+          },
+        },
+      },
+      ...message,
+    ];
+    setMessage(addChat);
+    setHistory(addTitle);
+  };
+
   return (
     <div className="chatGPT-UI">
-      <SideBar
-        chatTitle={ConversationTitle.items}
-        setSelectedChat={setSelectedChat}
+      <SideBar setSelectedChat={setSelectedChat} history={history} />
+      <ChatArea
+        message={message}
+        selectedChat={selectedChat}
+        handleSubmit={handleSubmit}
       />
-      <ChatArea chatUi={chatHistory} selectedChat={selectedChat} />
     </div>
   );
 }
