2749d525baa9f0cdcff6830cd13a1ba72227d2ca
Lazarus1_3 <eshetealazar11@gmail.com>
2026-08-20 23:47:49 +0300

    SetText

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -5,7 +5,13 @@ import {
   MicrophoneIcon,
 } from "@phosphor-icons/react";
 import ChatMessage from "./ChatMessage";
-function ChatArea({ chatUi, selectedChat }) {
+function ChatArea({ message, selectedChat,handleSubmit }) {
+  console.log("see", message);
+  const [text, setText] = useState("");
+
+
+  
+
   return (
     <div className="chatArea">
       <div className="chatArea-1">
@@ -16,15 +22,25 @@ function ChatArea({ chatUi, selectedChat }) {
       </div>
       <div className="middleSection">
         <div id="chat-1">
-          <ChatMessage chatUi={chatUi} selectedChat={selectedChat} />
+          <ChatMessage message={message} selectedChat={selectedChat} />
         </div>
         <div className="typingSection" id="typingSection">
           <button className="plusButton">+</button>
-          <input className="typeInput" placeholder="Ask Anything" />
+          <input
+            value={text}
+            onChange={(d) => setText(d.target.value)}
+            className="typeInput"
+            placeholder="Ask Anything"
+          />
           <button className="voiceButton">
             <MicrophoneIcon size={32} />
           </button>
-          <button className="addButton">
+          <button
+            className="addButton"
+            onClick={() => {
+              handleSubmit(text);
+            }}
+          >
             <ArrowUpIcon size={32} />
           </button>
         </div>
